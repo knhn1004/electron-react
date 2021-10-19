034f590ba7c7865259021cb96269f73e74e5a1ed
Chia-hong Chou <chia-hongchou@Olivers-MacBook-Pro.local>
2021-10-19 15:50:53 +0800

    init

---
+++ public/main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {},
+  });
+  win.loadURL('http://localhost:3000');
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
